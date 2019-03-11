88b4303c0af569cb06ddbcd27ae3469b9f8a4819
rickyrsn <rickzz.corby@gmail.com>
2019-03-11 10:43:15 +0700

    change index js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n Ini NodeJS Docker');
+  res.send('Hello world\n NodeJS Docker');
 });
 
 app.listen(PORT, HOST);
